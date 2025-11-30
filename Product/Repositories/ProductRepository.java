cdc697bfeb50c37ce75d4931639846db0084863d
Ghaida02a <ggmskaj@gmail.com>
2025-11-30 14:20:55 +0400

    Added Inventory Management System (Product) CRUD methods

---
+++ Product/Repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.codeLine.Product.Repositories;
+
+import com.codeLine.Product.Entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+}
