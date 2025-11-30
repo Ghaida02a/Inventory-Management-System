a081a2c69e7d34778a60146eeee472c894fcda55
Ghaida02a <ggmskaj@gmail.com>
2025-11-30 14:30:32 +0400

    Deleted Product file from main branch

--- Product/Repositories/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codeLine.Product.Repositories;
-
-import com.codeLine.Product.Entities.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-}
